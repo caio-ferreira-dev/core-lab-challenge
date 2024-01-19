762ed7f101cdb024b68929745137e88d56436046
caio-ferreira-dev <caioferreirasdj@gmail.com>
2024-01-19 08:48:25 -0300

    frontend(updated newNote component)

--- app/frontend/components/NewNote.tsx
+++ app/frontend/components/NewNote.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import styles from '../src/styles/newNote.module.scss'
 import Image from 'next/image'
 import { Note } from '../types/note'
+import axios from 'axios'
 
 const defaultState = {
     id: 0,
@@ -9,23 +10,63 @@ const defaultState = {
     name: '',
     content: '',
     favorite: false,
-    color: '',
+    color: 'white',
     }
 
 export default function NewNote() {
     const [note, setNote] = useState<Note>(defaultState)
- 
+    const [formError, setFormError] = useState('')
+    const textAreaRef = useRef<HTMLTextAreaElement>(null)
+
+    useEffect(() => {
+        if (textAreaRef.current) {
+            textAreaRef.current.style.height = 'auto';
+            textAreaRef.current.style.height = textAreaRef.current.scrollHeight + 'px';
+        } 
+    }, [note.content])
+
+    const handleTitleChange = (e: ChangeEvent<HTMLInputElement> ) => {
+        setNote({...note, name: e.target.value})
+    } 
+    const handleFavoriteChange = () => {
+        setNote({...note, favorite: !note.favorite})
+    }
+    const handleNoteSave = async () => {
+        if(note.name.length < 1) {
+            setFormError('Adicione um Título à nota.')
+            return
+        }
+        if(note.content.length < 1) {
+            setFormError('Adicione conteúdo à nota.')
+            return
+        }
+        setFormError('')
+        const { id, created_at, ...data } = note
+        const response = await axios.post('http://localhost:5001/notes', data)
+        if(response.data.statusCode === 409) {
+            setFormError('Já existe uma nota com este nome.')
+            return
+        }
+        setNote(defaultState)
+
+    }
     return (
         <div className={styles.container}>
             <div className={styles.titleContainer}>
-                <input type="text" placeholder='Titulo' value={note.name} onChange={(e) => {setNote({...note, name: e.target.value})}}/>
+                <input type="text" placeholder='Título' value={note.name} onChange={handleTitleChange}/>
                 {note.favorite ? 
-                    <Image alt='Favorite icon' src={'/images/favorite_icon.png'} width={16} height={16} onClick={() => {setNote({...note, favorite: !note.favorite})}}></Image> :
-                    <Image alt='Favorite icon' src={'/images/not_favorite_icon.png'} width={16} height={16} onClick={() => {setNote({...note, favorite: !note.favorite})}}></Image>
+                    <Image alt='Favorite icon' src={'/images/favorite_icon.png'} width={16} height={16} onClick={handleFavoriteChange}></Image> :
+                    <Image alt='Favorite icon' src={'/images/not_favorite_icon.png'} width={16} height={16} onClick={handleFavoriteChange}></Image>
                 }
             </div>
-            <input type="textarea" placeholder='Criar nota...' value={note.content} onChange={(e) => {setNote({...note, content: e.target.value })}}/>
-            
+            <textarea placeholder='Criar nota...' value={note.content} ref={textAreaRef} onChange={(e) => {setNote({...note, content: e.target.value })}}/>
+            <div className={styles.buttonDiv}>
+                {formError ? 
+                    <span>{formError}</span> : 
+                    ''
+                }
+                <button onClick={handleNoteSave}></button>
+            </div>
         </div>
     )
 }
